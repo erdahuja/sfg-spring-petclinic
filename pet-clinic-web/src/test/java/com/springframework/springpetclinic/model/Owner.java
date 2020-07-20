6e5bbff7e29ab3d38ba36e442702e67b11dce1a7
erdahuja <er.d.ahuja@gmail.com>
2020-07-20 08:41:43 +0530

    remove outtdated packages

--- pet-clinic-web/src/test/java/com/springframework/springpetclinic/model/Owner.java
+++
@@ -1,4 +0,0 @@
-package com.springframework.springpetclinic.model;
-
-public class Owner extends Person {
-}
--- pet-clinic-web/src/test/java/com/springframework/springpetclinic/model/Vet.java
+++
@@ -1,4 +0,0 @@
-package com.springframework.springpetclinic.model;
-
-public class Vet extends Person {
-}
