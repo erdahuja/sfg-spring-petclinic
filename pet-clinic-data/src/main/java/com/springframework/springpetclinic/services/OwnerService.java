72cebd21b01b6fbb778e657a390f1194d1f74147
erdahuja <er.d.ahuja@gmail.com>
2020-07-20 09:17:41 +0530

    add basic services for models

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/OwnerService.java
@@ -0,0 +1,11 @@
+package com.springframework.springpetclinic.services;
+
+import com.springframework.springpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
